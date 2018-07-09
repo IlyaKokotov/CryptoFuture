21fc435d38cbd9cf13e9212ce220a9d0e6f1e9c1
EllNino <kokotovi@yandex.ru>
2018-07-09 22:25:44 +0500

    Some edits

--- src/main/java/ru/CryptoFuture/validator/UserValidator.java
+++ src/main/java/ru/CryptoFuture/validator/UserValidator.java
@@ -29,29 +29,29 @@ public class UserValidator implements Validator {
     @Override
     public void validate(Object o, Errors errors) {
 
-        User user = (User) o;
+        User validateUser = (User) o;
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "Required");
-        if(user.getUsername().length() < 8 || user.getUsername().length() > 32) {
+        if(validateUser.getUsername().length() < 8 || validateUser.getUsername().length() > 32) {
 
             errors.rejectValue("username", "Size.userForm.username");
 
         }
 
-        if(userService.findByUsername(user.getUsername()) != null) {
+        if(userService.findByUsername(validateUser.getUsername()) != null) {
 
             errors.rejectValue("username", "Duplicate.userForm.username");
 
         }
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "Required");
-        if(user.getPassword().length() < 8 || user.getPassword().length() > 32) {
+        if(validateUser.getPassword().length() < 8 || validateUser.getPassword().length() > 32) {
 
             errors.rejectValue("password", "Size.userForm.password");
 
         }
 
-        if(!user.getConfirmPassword().equals(user.getPassword())) {
+        if(!validateUser.getConfirmPassword().equals(validateUser.getPassword())) {
             errors.rejectValue("confirmPassword", "Different.userForm.password");
         }
     }
